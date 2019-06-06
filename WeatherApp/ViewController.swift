2c2acd470ec2abb70c7616602fdf7092e56aabb5
dimas <dimas.syuhada@infinetworks.com>
2019-06-06 22:37:03 +0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by admin on 06/06/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
